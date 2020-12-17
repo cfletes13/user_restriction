1f280af0be8cdc60cf66da7f3c31c096bf034af7
Carlos Fletes <carlos.fletes@marvelsa.com>
2020-12-17 10:30:06 -0600

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'User Restriction For Creating Product And Partner',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'User Restriction For Creating Product And Partner Product Restriction on Users partner Restriction on Users user_product_restriction product creation Restriction partner creation Restriction product creation restrict product create restrict partner create product access hide invisible any menu hide invisible any hide invisible menu hide any invisible hide any menu invisible hide generate barcode product auto Restrict Read Only User Hide Any Menu Restrict User Menus multi level approve three level approve Tripple Approve Purchase Tripple Approval Project Checklist Task Checklist',
+    'description': "User Restriction For Creating Product And Partner",
+    'depends': ['base', 'product'],
+    'data': [
+        'security/user_restriction_security.xml',
+    ],
+    'images': ['static/description/user_restriction_banner.png'],
+    'license': "OPL-1",
+    'price': 14.99,
+    'currency': "USD",
+    'auto_install': False,
+    'installable': True,
+    'application': False
+}
